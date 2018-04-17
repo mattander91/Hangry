502ae59c2f9b339ec14283158ed737e2592e8bb6
mattander91 <mattander91@gmail.com>
2018-04-16 17:31:18 -0700

    Updated README

--- client/components/about.jsx
+++ client/components/about.jsx
@@ -5,7 +5,7 @@ const About = () => (
     <div id="hangry-word-logo-about">About</div>
     <div className="about">
       <p style={{'font-size': '25px'}}>About Hangry</p>
-      <p>Tired of reading through menus online to see if they have what you're craving? Look no further! Simply search for the food you want ("chicken wings", "meatball sandwich", etc) and enter your city or zip code. If we currently have the relevant restaurants in th database, we'll return specific menu items from restaurants that match your search within 10 miles. If you can't seem to find what you're looking for, please read below.
+      <p>Tired of reading through menus online to see if they have what you're craving? Look no further! Simply search for the food you want ("chicken wings", "meatball sandwich", etc) and enter your city or zip code. If we currently have the relevant restaurants in the database, we'll return specific menu items from restaurants that match your search within 10 miles. If you can't seem to find what you're looking for, please read below.
       </p>
       <p style={{'font-size': '25px'}}>Hangry is supported in select cities of the following states</p>
       <p>Arizona, California, Colorado, Connecticut, Delaware, District of Columbia, Florida, Georgia, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana, Maryland, Massachusetts, Michigan, Minnesota, Missouri, Montana, Nebraska, Nevada, New Jersey, New Mexico, New York, North Carolina, North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania Rhode Island, South Carolina, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia, Wisconsin.
